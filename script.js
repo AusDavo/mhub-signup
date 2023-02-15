f00c3c40ed7441f7cda5f1f6cda3333985050ef6
David Pinkerton <david@dpinkerton.com>
2023-02-15 12:34:58 +1000

    Add a check that the password matches the confirmation field

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 function validatePassword() {
-    const password = document.getElementById("password").value;
-    const confirmPassword = document.getElementById("password-conf").value;
-    
-    if (password !== confirmPassword) {
-      alert("Passwords do not match.");
-      return false;
-    }
-    
-    return true;
-  }
-  
+const password = document.getElementById("password").value;
+const confirmPassword = document.getElementById("password-conf").value;
+const confirmPasswordField = document.getElementById("password-conf");
+
+if (password !== confirmPassword) {
+    confirmPasswordField.setCustomValidity("Passwords do not match");
+    confirmPasswordField.validity.valid = False;
+    return false;
+} else {
+    confirmPasswordField.setCustomValidity("");
+    confirmPasswordField.validity.valid = True;
+}
+
+return true;
+}
+
+const passwordField = document.getElementById("password");
+const confirmPasswordField = document.getElementById("password-conf");
+
+passwordField.addEventListener("input", validatePassword);
+confirmPasswordField.addEventListener("input", validatePassword);
